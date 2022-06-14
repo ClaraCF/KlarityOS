5f9f717646156b15a8b5a0d8463bcdea860534b7
Clara <ClaraCf7@proton.me>
2022-06-13 21:43:39 -0500

    Test new features

--- src/impl/kernel/main.c
+++ src/impl/kernel/main.c
@@ -1,8 +1,15 @@
 #include "print.h"
+#include "io.h"
 
 void kernel_main(){
     // Welcome message
     print_clear();
     print_set_color(PRINT_COLOR_PURPLE, PRINT_COLOR_BLACK);
     print_str("Welcome to KlarityOS!\n\n");
+
+    print_str("Number: ");
+    print_num(1234567890);
+
+    print_str("\n\nHex value: ");
+    print_hex(0xb8000);
 }
